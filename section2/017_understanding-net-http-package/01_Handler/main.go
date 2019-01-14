fe34c56e7af37aa101b043efdbb663b1ca3258bf
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-01-14 17:49:33 -0500

    This implements the Handler Interface. 100% implicit

--- section2/017_understanding-net-http-package/01_Handler/main.go
+++ section2/017_understanding-net-http-package/01_Handler/main.go
@@ -7,6 +7,7 @@ import (
 
 type hotdog int
 
+// implicitly implements the Handler Interface
 func (m hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Any code you want in this func")
 }
