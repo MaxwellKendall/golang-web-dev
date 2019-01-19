88f5539286db4a6fe3c36408add2e9ddcbb0c85d
Maxwell Kendall <maxwellkendall@Maxwells-MacBook-Pro.local>
2019-01-19 16:42:11 -0500

    fixing issue with setting cookie "Count" value

--- 029_cookies/03_hands-on/main.go
+++ 029_cookies/03_hands-on/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -8,31 +9,34 @@ import (
 
 func main() {
 	http.HandleFunc("/", index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.HandleFunc("/increment", increment)
 	http.ListenAndServe(":8080", nil)
 }
 
-func index(rw http.ResponseWriter, req *http.Request) {
-	http.SetCookie(rw, &http.Cookie{Name: "count", Value: "0"})
+func index(res http.ResponseWriter, req *http.Request) {
+	// http.SetCookie(res, &http.Cookie{Name: "count", Value: "0"})
+	io.WriteString(res, "Nicely done...?")
 }
 
-func increment(rw http.ResponseWriter, req *http.Request) {
+func increment(res http.ResponseWriter, req *http.Request) {
 	counter, err := req.Cookie("count")
 
-	if err != nil {
-		fmt.Fprint(rw, err)
+	if err == http.ErrNoCookie {
+		counter = generateCounterCookie(0)
+	} else if err != nil {
+		fmt.Fprint(res, err)
 	}
 
 	count, err := strconv.Atoi(counter.Value)
 
 	if err != nil {
-		fmt.Fprint(rw, err)
+		fmt.Fprint(res, err)
 	}
 
 	count++
 
-	http.SetCookie(rw, generateCounterCookie(count))
-
+	http.SetCookie(res, generateCounterCookie(count))
 }
 
 func generateCounterCookie(count int) *http.Cookie {
